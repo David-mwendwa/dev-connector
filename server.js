4f8b5b9abb8a6da9c45ebcbae53ccecf1c03f2e5
David Mwendwa <klatzdavie19@gmail.com>
2021-07-08 23:37:11 +0300

    feat: implement jwt

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const express = require('express');
 
 // Connect database
-require('./config/db')()
+require('./config/db')();
 
 const app = express();
 
 // Init middleware
 app.use(express.json({ limit: '10kb' }));
 
-app.get('/', (req, res) => res.send('API Running'))
+app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
-
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
