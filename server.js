1d787f7b0217575a79ec76cdc7092c03cd7db14a
David Mwendwa <klatzdavie19@gmail.com>
2021-07-08 17:43:50 +0300

    feat: route files with express router

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose')
 const express = require('express');
 
-// connect database
+// Connect database
 require('./config/db')()
 
 const app = express();
 
 app.get('/', (req, res) => res.send('API Running'))
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000
 
