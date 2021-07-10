f44ec61add6edab422a7d9cd690c26dd4a9d36b3
David Mwendwa <klatzdavie19@gmail.com>
2021-07-10 18:02:24 +0300

    feat: implement deletion of posts
    Only authorized to the post owners

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -69,4 +69,29 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: 'post not found' });
+
+    if (post.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: 'User not authorized' });
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId')
+      return res.status(404).json({ msg: 'post not found' });
+    res.status(500).send('server error');
+  }
+});
+
 module.exports = router;
