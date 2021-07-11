1f07b18d8252e50ec2e63152d087533140240e19
David Mwendwa <klatzdavie19@gmail.com>
2021-07-11 23:09:48 +0300

    feat: design a landing page

--- client/src/components/Layout/Landing/Landing.js
+++ client/src/components/Layout/Landing/Landing.js
@@ -1,12 +1,14 @@
+import './Landing.scss';
+
 const Landing = () => {
   return (
-    <div>
-      <h1>Developer Connector</h1>
-      <p>
+    <div className='landing'>
+      <h1 className='landing__heading'>Developer Connector</h1>
+      <p className='landing__para'>
         Create a developer profile or portfolio, share posts and get help
         connect with fellow developers
       </p>
-      <div class='buttons'>
+      <div className='buttons'>
         <button>Sign up</button>
         <button>Login</button>
       </div>
