f08ea1a71245d1c65e706a85e56fb348e76c1fc4
David Mwendwa <klatzdavie19@gmail.com>
2021-07-30 17:03:20 +0300

    feat: add postReducer and its actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux';
 import alertReducer from './alert';
 import authReducer from './auth';
 import profileReducer from './profile'
+import postReducer from './post'
 
 export default combineReducers({
   alert: alertReducer,
   auth: authReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
+
