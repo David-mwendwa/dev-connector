f7d50c1a8c4862ee74951f954e008decadf3a3d4
David Mwendwa <klatzdavie19@gmail.com>
2021-08-11 20:02:57 +0300

    feat: display comments on postItem component

--- models/Post.js
+++ models/Post.js
@@ -1,49 +1,98 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  text: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-  },
-  avatar: {
-    type: String,
-  },
-  likes: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-      },
-    },
-  ],
-  comments: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-      },
-      text: {
-        type: String,
-        required: true,
-      },
-      date: {
-        type: Date,
-        default: Date.now,
-      },
-    },
-  ],
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+// const PostSchema = new Schema({
+  // user: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'users',
+  // },
+  // text: {
+  //   type: String,
+  //   required: true,
+  // },
+  // name: {
+  //   type: String,
+  // },
+  // avatar: {
+  //   type: String,
+  // },
+  // likes: [
+  //   {
+  //     user: {
+  //       type: Schema.Types.ObjectId,
+  //       ref: 'users',
+  //     },
+  //   },
+  // ],
+  // comments: [
+  //   {
+  //     user: {
+  //       type: Schema.Types.ObjectId,
+  //       ref: 'User',
+  //     },
+  //     text: {
+  //       type: String,
+  //       required: true,
+  //     },
+  //     date: {
+  //       type: Date,
+  //       default: Date.now,
+  //     },
+  //   },
+  // ],
+  // date: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
+// });
 
 //TODO: fix this model export
+// const mongoose = require('mongoose');
+
+// const PostSchema = new mongoose.Schema({
+//   user: {
+//     type: Schema.Types.ObjectId,
+//     ref: 'User',
+//   },
+//   text: {
+//     type: String,
+//     required: true,
+//   },
+//   name: {
+//     type: String,
+//   },
+//   avatar: {
+//     type: String,
+//   },
+//   likes: [
+//     {
+//       user: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'User',
+//       },
+//     },
+//   ],
+//   comments: [
+//     {
+//       user: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'User',
+//       },
+//       text: {
+//         type: String,
+//         required: true,
+//       },
+//       date: {
+//         type: Date,
+//         default: Date.now,
+//       },
+//     },
+//   ],
+//   date: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+// module.exports = Post = mongoose.model('Post', PostSchema);
+
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 
-const Post = require('../../models/Post');
+const Post = require('../../models/PostModel');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
